26605622d90b00c6d5849d689c3747638faad839
dukewp <wproyal.duke@gmail.com>
2021-10-06 15:25:35 +0400

    fine tunning

--- extensions/particles.php
+++ extensions/particles.php
@@ -8,48 +8,11 @@ class Particles {
 	private static $_instance = null;
 
 	public function __construct() {
-        add_action( 'wp_enqueue_scripts', [ $this, 'wpr_add_particles' ] );
-		// add_action( 'elementor/editor/wp_head', [ $this, 'wpr_add_particles' ] );
 		add_action( 'elementor/element/after_section_end', [ $this, 'register_controls' ], 10, 3 );
-
 		add_action( 'elementor/section/print_template', [ $this, '_print_template' ], 10, 2 );
-
 		add_action( 'elementor/frontend/section/before_render', [ $this, '_before_render' ], 10, 1 );
-
+        add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
 	}
-
-	public function _before_render( $element ) {
-		if ( $element->get_name() !== 'section' ) {
-			return;
-		}
-
-		$settings = $element->get_settings();
-		if ( $settings['wpr_enable_particles'] === 'yes' ) {
-			$element->add_render_attribute( '_wrapper', 'data-wpr-particles', $settings['wpr_particle_json'] );
-		}
-	}
-
-	public function _print_template( $template, $widget ) {
-		if ( $widget->get_name() !== 'section' ) {
-			return $template;
-		}
-
-		$old_template = $template;
-		ob_start();
-
-		?>
-
-		<div class="wpr-particle-wrapper" id="wpr-particle-{{ view.getID() }}" data-wpr-particles-editor=" {{ settings.wpr_particle_json }}"></div>
-
-		<?php
-
-		$slider_content = ob_get_contents();
-		ob_end_clean();
-		$template = $slider_content . $old_template;
-		
-		return $template;
-	}
-
 
 	public function register_controls( $element, $section_id, $args ) {
 
@@ -93,7 +56,36 @@ class Particles {
         }
     }
 
-    public function wpr_add_particles() {
+	public function _print_template( $template, $widget ) {
+		if ( $widget->get_name() !== 'section' ) {
+			return $template;
+		}
+
+		$old_template = $template;
+		ob_start();
+
+		echo '<div class="wpr-particle-wrapper" id="wpr-particle-{{ view.getID() }}" data-wpr-particles-editor=" {{ settings.wpr_particle_json }}"></div>';
+
+		$particles_content = ob_get_contents();
+		
+		ob_end_clean();
+
+		return $template . $particles_content;
+	}
+
+	public function _before_render( $element ) {
+		if ( $element->get_name() !== 'section' ) {
+			return;
+		}
+
+		$settings = $element->get_settings();
+
+		if ( $settings['wpr_enable_particles'] === 'yes' ) {
+			$element->add_render_attribute( '_wrapper', 'data-wpr-particles', $settings['wpr_particle_json'] );
+		}
+	}
+
+    public function enqueue_scripts() {
 		wp_enqueue_script( 'wpr-particles', WPR_ADDONS_URL . 'assets/js/lib/particles/particles.js', [ 'jquery' ], '3.0.6', true );
 	}
 
