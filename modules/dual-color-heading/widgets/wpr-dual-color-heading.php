145248cd321ea508a27e9fbc5b0a7e1498a62c29
RUBEUSHAGRID13 <geobeatnic@gmail.com>
2022-04-18 14:36:07 +0400

    c

--- modules/dual-color-heading/widgets/wpr-dual-color-heading.php
+++ modules/dual-color-heading/widgets/wpr-dual-color-heading.php
@@ -98,6 +98,24 @@ class Wpr_Dual_Color_Heading extends Widget_Base {
 		);
 
 		$this->add_control(
+			'dual_heading_tag',
+			[
+				'label' => esc_html__( 'Heading HTML Tag', 'wpr-addons' ),
+				'type' => Controls_Manager::SELECT,
+				'options' => [
+					'h1' => 'H1',
+					'h2' => 'H2',
+					'h3' => 'H3',
+					'h4' => 'H4',
+					'h5' => 'H5',
+					'h6' => 'H6',
+					'div' => 'Div'
+				],
+				'default' => 'h2'
+			]
+		);
+
+		$this->add_control(
 			'primary_heading',
 			[
 				'label'   => __('Primary Heading', 'wpr-addons'),
