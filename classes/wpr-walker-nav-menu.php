f07ec5a31e409f597c50b4d76340c1c5832cf79e
RUBEUSHAGRID13 <geobeatnic@gmail.com>
2022-04-21 14:39:43 +0400

    walker-dump

--- classes/wpr-walker-nav-menu.php
+++ classes/wpr-walker-nav-menu.php
@@ -11,8 +11,6 @@ class Wpr_Walker_Nav_Menu extends \Walker_Nav_Menu {
     function start_lvl(&$output, $depth = 0, $args = null) {
         $indent = str_repeat("\t", $depth);
         $submenu = ($depth > 0) ? ' wpr-mega-menu-test' : '';
-        var_dump($depth > 0);
-        var_dump($submenu);
         $output .= "\n$indent<ul class=\"drodown-menu$submenu depth_$depth\">\n";
     }
 
