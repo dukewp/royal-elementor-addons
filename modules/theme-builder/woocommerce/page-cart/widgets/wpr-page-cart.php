62d2d23ebeede0e237832ad74e910399c8d25a4c
RUBEUSHAGRID13 <geobeatnic@gmail.com>
2022-07-15 14:00:30 +0400

    cc

--- modules/theme-builder/woocommerce/page-cart/widgets/wpr-page-cart.php
+++ modules/theme-builder/woocommerce/page-cart/widgets/wpr-page-cart.php
@@ -1005,6 +1005,7 @@ class Wpr_Page_Cart extends Widget_Base {
 				'type' => Controls_Manager::COLOR,
 				'selectors' => [
 					'{{WRAPPER}} .input-text' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .input-text::placeholder' => 'color: {{VALUE}};',
 					'{{WRAPPER}} .woocommerce-shipping-calculator .input-text' => 'color: {{VALUE}};',
 					'{{WRAPPER}} .woocommerce-shipping-calculator select' => 'color: {{VALUE}};',
 					'{{WRAPPER}} .woocommerce-shipping-calculator span' => 'color: {{VALUE}};',
@@ -1834,6 +1835,15 @@ class Wpr_Page_Cart extends Widget_Base {
 				'selectors' => [
 					'{{WRAPPER}} .cart_totals table.shop_table td' => 'background-color: {{VALUE}}'
 				]
+			]
+		);
+
+		$this->add_control(
+			'cart_totals_table_shipping_calc',
+			[
+				'type' => Controls_Manager::HEADING,
+				'label' => esc_html__( 'Shipping Calculator', 'wpr-addons' ),
+				'separator' => 'before'
 			]
 		);
 
