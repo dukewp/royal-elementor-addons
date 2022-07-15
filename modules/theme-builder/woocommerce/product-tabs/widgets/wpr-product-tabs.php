1dcc59b302e110a48dabb52545b8b153ee25f0e9
dukewp <wproyal.duke@gmail.com>
2022-07-15 15:34:48 +0400

    c

--- modules/theme-builder/woocommerce/product-tabs/widgets/wpr-product-tabs.php
+++ modules/theme-builder/woocommerce/product-tabs/widgets/wpr-product-tabs.php
@@ -35,11 +35,30 @@ class Wpr_Product_Tabs extends Widget_Base {
 
 	public function get_keywords() {
 		return [ 'qq', 'product-tabs', 'product', 'tabs' ];//tmp
-	}	
+	}
 	
 	public function get_script_depends() {
 		return [ 'wc-single-product' ];
 	}
+	
+	public function add_control_tabs_position() {
+		$this->add_control(
+			'tabs_position',
+			[
+				'type' => Controls_Manager::SELECT,
+				'label' => esc_html__( 'Label Position', 'wpr-addons' ),
+				'default' => 'above',
+				'options' => [
+					'above' => esc_html__( 'Horizontal', 'wpr-addons' ),
+					'pro-lt' => esc_html__( 'Vertical Left (Pro)', 'wpr-addons' ),
+					'pro-rt' => esc_html__( 'Vertical Right (Pro)', 'wpr-addons' ),
+				],
+				'prefix_class' => 'wpr-tabs-position-',
+			]
+		);
+	}
+
+	public function add_controls_group_tabs_label_adjustments() {}
 
 	protected function register_controls() {
 
@@ -51,163 +70,12 @@ class Wpr_Product_Tabs extends Widget_Base {
 			]
 		);
 
-		$this->add_control(
-			'tabs_position',
-			[
-				'type' => Controls_Manager::SELECT,
-				'label' => esc_html__( 'Label Position', 'wpr-addons' ),
-				'default' => 'above',
-				'options' => [
-					'above' => esc_html__( 'Default', 'wpr-addons' ),
-					'left' => esc_html__( 'Left', 'wpr-addons' ),
-					'right' => esc_html__( 'Right', 'wpr-addons' ),
-				],
-				'prefix_class' => 'wpr-tabs-position-',
-			]
-		);
+		$this->add_control_tabs_position();
 
-		$this->add_control(
-            'tabs_hr_position',
-            [
-                'label' => esc_html__( 'Horizontal Align', 'wpr-addons' ),
-                'type' => Controls_Manager::CHOOSE,
-                'label_block' => false,
-                'default' => 'justify',
-                'options' => [
-                    'left' => [
-                        'title' => esc_html__( 'Left (Pro)', 'wpr-addons' ),
-                        'icon' => 'eicon-h-align-left',
-                    ],
-                    'center' => [
-                        'title' => esc_html__( 'Center (Pro)', 'wpr-addons' ),
-                        'icon' => 'eicon-h-align-center',
-                    ],
-                    'right' => [
-                        'title' => esc_html__( 'Right (Pro)', 'wpr-addons' ),
-                        'icon' => 'eicon-h-align-right',
-                    ],
-                    'justify' => [
-						'title' => esc_html__( 'Stretch', 'wpr-addons' ),
-						'icon' => 'eicon-h-align-stretch',
-					],
-                ],
-				'prefix_class' => 'wpr-tabs-hr-position-',
-				'render_type' => 'template',
-				'condition' => [
-					'tabs_position' => 'above',
-				],
-            ]
-        );
+		// Upgrade to Pro Notice
+		Utilities::upgrade_pro_notice( $this, Controls_Manager::RAW_HTML, 'product-tabs', 'tabs_position', ['pro-lt', 'pro-rt'] );
 
-		if ( ! wpr_fs()->can_use_premium_code() ) {
-			$this->add_control(
-	            'tabs_align_pro_notice',
-	            [
-					'raw' => 'Horizontal Align option is fully supported in the <strong><a href="https://royal-elementor-addons.com/?ref=rea-plugin-panel-tabs-upgrade-pro#purchasepro" target="_blank">Pro version</a></strong>',
-					// 'raw' => 'Horizontal Align option is fully supported in the <strong><a href="'. admin_url('admin.php?page=wpr-addons-pricing') .'" target="_blank">Pro version</a></strong>',
-					'type' => Controls_Manager::RAW_HTML,
-					'content_classes' => 'wpr-pro-notice',
-					'condition' => [
-						'tabs_hr_position!' => 'justify',
-					],
-				]
-	        );
-		}
-
-		$this->add_control(
-			'tabs_vr_position',
-			[
-				'label' => esc_html__( 'Vertical Align', 'wpr-addons' ),
-                'type' => Controls_Manager::CHOOSE,
-                'label_block' => false,
-                'default' => 'middle',
-                'options' => [
-                    'top' => [
-						'title' => esc_html__( 'Top', 'wpr-addons' ),
-						'icon' => 'eicon-v-align-top',
-					],
-					'middle' => [
-						'title' => esc_html__( 'Middle', 'wpr-addons' ),
-						'icon' => 'eicon-v-align-middle',
-					],
-					'bottom' => [
-						'title' => esc_html__( 'Bottom', 'wpr-addons' ),
-						'icon' => 'eicon-v-align-bottom',
-					],
-                ],
-                'selectors_dictionary' => [
-					'top' => 'flex-start',
-					'middle' => 'center',
-					'bottom' => 'flex-end'
-				],
-				'selectors' => [
-					'{{WRAPPER}} .wc-tabs-wrapper .wc-tabs' => 'justify-content: {{VALUE}};',
-				],
-				'condition' => [
-					'tabs_position!' => 'above',
-				],
-			]
-		);
-
-		$this->add_control( //TODO: change approach
-			'text_align',
-			[
-				'label' => esc_html__( 'Label Alignment', 'wpr-addons' ),
-				'type' => Controls_Manager::CHOOSE,
-				'default' => 'center',
-				'label_block' => false,
-				'options' => [
-					'left' => [
-						'title' => esc_html__( 'Left', 'wpr-addons' ),
-						'icon' => 'eicon-text-align-left',
-					],
-					'center' => [
-						'title' => esc_html__( 'Center', 'wpr-addons' ),
-						'icon' => 'eicon-text-align-center',
-					],
-					'right' => [
-						'title' => esc_html__( 'Right', 'wpr-addons' ),
-						'icon' => 'eicon-text-align-right',
-					],
-				],
-				// 'selectors_dictionary' => [
-				// 	'left' => 'flex-start',
-				// 	'center' => 'center',
-				// 	'right' => 'flex-end'
-				// ],
-				'selectors' => [
-					// '{{WRAPPER}} .wc-tabs li' => 'display: flex; align-items: {{VALUE}}; justify-content: {{VALUE}};',
-					'{{WRAPPER}} .wc-tabs li' => 'text-align: {{VALUE}};',
-					'{{WRAPPER}} .wc-tabs li a' => 'text-align: {{VALUE}};'
-				],
-			]
-		);
-
-		$this->add_responsive_control(
-			'tabs_width',
-			[
-				'label' => esc_html__( 'Label Width', 'wpr-addons' ),
-				'type' => Controls_Manager::SLIDER,
-				'size_units' => ['px'],
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 600,
-					],
-				],
-				'default' => [
-					'unit' => 'px',
-					'size' => 90,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .wc-tabs li' => 'min-width: {{SIZE}}px;',
-					'{{WRAPPER}} .wc-tabs li a' => 'min-width: {{SIZE}}px; display: block;'
-				],
-				'condition' => [
-					'tabs_hr_position!' => 'justify',
-				],
-			]
-		);
+		$this->add_controls_group_tabs_label_adjustments();
 
 		$this->add_control(
 			'tabs_style_divider',
